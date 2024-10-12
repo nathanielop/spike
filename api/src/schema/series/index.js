6ae8bc94173adc0325f9e99618c0726614199624
Nathaniel Pendy <nathanielp2402@gmail.com>
2024-10-11 20:15:10 -0500

    fix empty fiels

--- api/src/schema/series/index.js
+++ api/src/schema/series/index.js
@@ -1,14 +1,14 @@
 import createDbType from '#src/functions/create-db-type.js';
-import games from '#src/schema/series/games.js';
-import teams from '#src/schema/series/teams.js';
+// import games from '#src/schema/series/games.js';
+// import teams from '#src/schema/series/teams.js';
 
 export default createDbType({
   table: 'series',
   local: {
     format: 'format'
-  },
-  fields: {
-    games,
-    teams
   }
+  // fields: {
+  //   games,
+  //   teams
+  // }
 });
