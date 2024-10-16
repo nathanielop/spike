b7d4c610cf5a7fa74152d01b48b0433a2e314729
Nathaniel Pendy <nathanielp2402@gmail.com>
2024-10-16 07:23:14 -0500

    fix class

--- www/src/components/thunder-dome/warrior.js
+++ www/src/components/thunder-dome/warrior.js
@@ -3,7 +3,7 @@ import UserAvatar from '#src/components/user-avatar.js';
 export default ({ profile }) => (
   <UserAvatar
     player={profile}
-    className='h-[16rem] square'
+    className='h-[16rem] aspect-square'
     textClassName='text-base'
   />
 );
