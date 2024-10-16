49c8d80e63eb9d7a133ecc8f8798f7c56f421a37
Nathaniel Pendy <nathanielp2402@gmail.com>
2024-10-16 06:59:30 -0500

    square

--- www/src/components/thunder-dome/warrior.js
+++ www/src/components/thunder-dome/warrior.js
@@ -3,7 +3,7 @@ import UserAvatar from '#src/components/user-avatar.js';
 export default ({ profile }) => (
   <UserAvatar
     player={profile}
-    className='h-[16rem]'
+    className='h-[16rem] square'
     textClassName='text-base'
   />
 );
