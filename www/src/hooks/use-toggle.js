facf8891ecadb1441af3bc2233960b4706267c6e
Nathaniel Pendy <nathanielp2402@gmail.com>
2024-10-22 19:10:18 -0500

    durr

--- www/src/hooks/use-toggle.js
+++ www/src/hooks/use-toggle.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'endr';
+import { useCallback, useEffect, useState } from 'endr';
 
 export default initial => {
   const [val, setVal] = useState(!!initial);
@@ -6,6 +6,8 @@ export default initial => {
   const toggle = useCallback(() => setVal(v => (v ? false : true)), [setVal]);
   const open = useCallback(() => setVal(true), [setVal]);
   const close = useCallback(() => setVal(false), [setVal]);
+
+  useEffect(() => setVal(!!initial), [initial]);
 
   return [val, open, close, toggle];
 };
