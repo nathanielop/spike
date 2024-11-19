170a69cdb35e3e48f80dd1c5a3285ab2e572d7bc
Nathaniel Pendy <n@thaniel.dev>
2024-11-19 13:59:35 -0600

    fidlar

--- api/src/janitor-types/cancel-long-running-games.js
+++ api/src/janitor-types/cancel-long-running-games.js
@@ -1,2 +1,23 @@
+import pave from 'pave';
+
+import db from '#src/constants/db.js';
+import createLoad from '#src/functions/create-load.js';
+import schema from '#src/schema/index.js';
+
 // Cancel games older than 1 hour
-export default async () => {};
+export default async () => {
+  const longRunningGames = await db
+    .select('id')
+    .from('games')
+    .whereNull('completedAt')
+    .where('createdAt', '<', new Date(Date.now() - 1000 * 60 * 60));
+
+  for (const { id } of longRunningGames) {
+    await pave.execute({
+      query: { deleteGame: { $: { id } } },
+      context: { load: createLoad(), player: { isAdmin: true } },
+      schema,
+      type: 'root'
+    });
+  }
+};
