1992015c6882f7d7f814ff6e5b1000b6b2830a17
Nathaniel Pendy <n@thaniel.dev>
2025-02-24 11:09:17 -0600

    just stop rolling until have more time to fix

--- api/src/janitor-types/index.js
+++ api/src/janitor-types/index.js
@@ -1,4 +1,5 @@
 import cancelLongRunningGames from '#src/janitor-types/cancel-long-running-games.js';
-import rollSeason from '#src/janitor-types/roll-season.js';
+// import rollSeason from '#src/janitor-types/roll-season.js';
 
-export default { cancelLongRunningGames, rollSeason };
+// export default { cancelLongRunningGames, rollSeason };
+export default { cancelLongRunningGames };
