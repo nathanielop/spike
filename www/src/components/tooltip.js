6c7cead052c02b6f95caa29b6490c7a2a1252dc6
Nathaniel Pendy <n@thaniel.dev>
2025-04-21 13:19:01 -0500

    fix tooltip

--- www/src/components/tooltip.js
+++ www/src/components/tooltip.js
@@ -1,8 +1,10 @@
 export default ({ tooltip, children }) => (
   <div className='relative group/tooltip'>
     {children}
-    <div className='absolute z-20 hidden top-[-100%] group-hover/tooltip:block bg-black text-white text-xs p-1 rounded-md whitespace-nowrap'>
-      {tooltip}
-    </div>
+    {tooltip && (
+      <div className='absolute z-20 hidden top-[-100%] left-[50%] group-hover/tooltip:block bg-black text-white text-xs p-1 rounded-md whitespace-nowrap'>
+        {tooltip}
+      </div>
+    )}
   </div>
 );
