80c28dfca7d1cd3a00d43d055d77fd42ff55a009
Nathaniel Pendy <n@thaniel.dev>
2025-05-14 14:22:49 -0500

    stop benching people

--- api/src/janitor-types/index.js
+++ api/src/janitor-types/index.js
@@ -1,6 +1,7 @@
-import benchInactivePlayers from '#src/janitor-types/bench-inactive-players.js';
+// import benchInactivePlayers from '#src/janitor-types/bench-inactive-players.js';
 import cancelLongRunningGames from '#src/janitor-types/cancel-long-running-games.js';
 // import rollSeason from '#src/janitor-types/roll-season.js';
 
 // export default { cancelLongRunningGames, rollSeason };
-export default { benchInactivePlayers, cancelLongRunningGames };
+// export default { benchInactivePlayers, cancelLongRunningGames };
+export default { cancelLongRunningGames };
