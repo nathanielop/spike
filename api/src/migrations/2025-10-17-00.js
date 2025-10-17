84e72acabbc5cbaafc54d675f4d97bae67a77c6d
Nathaniel Pendy <n@thaniel.dev>
2025-10-17 15:25:06 -0500

    copypasta

--- api/src/migrations/2025-10-17-00.js
+++ api/src/migrations/2025-10-17-00.js
@@ -2,11 +2,11 @@ export default {
   up: async tx =>
     await tx.schema.table('bets', t => {
       t.bigint('amount').notNullable().alter();
-      t.bigint('paidOutAmount').notNullable().alter();
+      t.bigint('paidOutAmount').alter();
     }),
   down: async tx =>
     await tx.schema.table('bets', t => {
       t.integer('amount').notNullable().alter();
-      t.integer('paidOutAmount').notNullable().alter();
+      t.integer('paidOutAmount').alter();
     })
 };
